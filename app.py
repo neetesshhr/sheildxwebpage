994de6c35afcb5edc3903bf721e5309f6708c2ad
neetesshhr <rijalboy.94@gmail.com>
2023-01-19 21:55:05 +0545

    final version from the my side

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from flask import Flask, request, render_template
 import os
+import psycopg2
 # from flask_bootstrap import Bootstrap
 FOLDERS = os.path.join('static', 'imgs')
 app= Flask(__name__)
 # Bootstrap(app)
 
+
+def get_db_connection():
+    conn = psycopg2.connect(host='database-2.ciluhbuaflhg.us-west-2.rds.amazonaws.com',
+                            database='flask',
+                            user='postgres',
+                            password='admin123')
+    
+    return conn
+
 app.config['UPLOAD_FOLDER'] =  FOLDERS
 
-@app.route('/')
-@app.route('/home/')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/home/', methods=('GET', 'POST'))
 def main():
     filename = os.path.join(app.config['UPLOAD_FOLDER'])
+    if request.method == 'POST':
+        name = request.form.get("name")
+        companyName = request.form.get("cname")
+        email = request.form.get('email')
+        comments = request.form.get('comment')
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO bookdemo (Name, email, company_name, comment)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (name, email, companyName, comments))
+        conn.commit()
+        cur.close()
+        conn.close()
+        # return render_template("index.html")
     return render_template("index.html")
 @app.route('/')
 @app.route('/about/')
@@ -22,15 +47,39 @@ def about():
 def services():
     return render_template('services.html')
 
-@app.route('/')
-@app.route('/pricing/')
-def pricing():
-    return render_template('pricing.html')
+
 
 @app.route('/')
-@app.route('/contact/')
+@app.route('/contact/', methods=('GET', 'POST'))
 def contact():
+    # filename = os.path.join(app.config['UPLOAD_FOLDER'])
+    if request.method == 'POST':
+        name = request.form.get("name")
+        companyName = request.form.get("cname")
+        email = request.form.get('email')
+        comments = request.form.get('comment')
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO bookdemo (Name, email, company_name, comment)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (name, email, companyName, comments))
+        conn.commit()
+        cur.close()
+        conn.close()
     return render_template('contact.html')
+
+@app.route('/')
+@app.route('/careers/')
+def careers():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM careers;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()    
+    
+    return render_template('careers.html', books=books)
 
 
 
--- static/js/function.js
+++ static/js/function.js
@@ -10,3 +10,31 @@ function mobileMenu() {
   menuList.classList.toggle("inactive");
   main.classList.toggle('main-filter')
 }
+
+var qoutes = [
+  "“Cyber-Security is much more than a matter of IT.” ― Stephane Nappo",
+  "“Passwords are like underwear: do’t let people see it, change it very often, and you shouldn’t share it with strangers.” – Chris Pirillo",
+  "“I really think that if we change our own approach and thinking about what we have available to us, that is what will unlock our ability to truly excel in security. It’s a perspectives exercise. What would it look like if abundance were the reality and not resource constraint?”  — Greg York",
+  "“It takes 20 years to build a reputation and few minutes of cyber-incident to ruin it.” – Stephane Nappo",
+  "“As we’ve come to realize, the idea that security starts and ends with the purchase of a prepackaged firewall is simply misguided.” – Art Wittmann",
+  "“As cybersecurity leaders, we have to create our message of influence because security is a culture and you need the business to take place and be part of that security culture.”  — Britney Hommertzheim",
+  "“We discovered in our research that insider threats are not viewed as seriously as external threats, like a cyberattack. But when companies had an insider threat, in general, they were much more costly than external incidents. This was largely because the insider that is smart has the skills to hide the crime, for months, for years, sometimes forever.”  — Dr. Larry Ponemon",
+  "“The knock-on effect of a data breach can be devastating for a company. When customers start taking their business—and their money—elsewhere, that can be a real body blow.” – Christopher Graham"
+]
+
+
+  var randomNumber = Math.floor(Math.random() * qoutes.length);
+  document.getElementById('quotesdisplay').innerHTML = qoutes[randomNumber];
+
+  function todayDate(){
+    var d = new Date();
+    var n = d.getFullYear() + "  ";
+    return document.getElementById("date").innerHTML = n;
+  }
+
+$(document).ready(function() {
+    $('#bookModal').modal({
+      show: false,
+      backdrop: 'static'
+    });
+  });
